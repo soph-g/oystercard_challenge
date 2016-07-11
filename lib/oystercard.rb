45509dec2839c8800ef50a37e6c2d4642220dbc7
Sophie Gill <sophie-gill@hotmail.com>
2016-07-11 18:38:54 +0100

    added maximum balance check to oystercard top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,20 @@
 class OysterCard
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize(balance = 0)
     @balance = balance
   end
+
   def top_up amount
-@balance += amount
+    fail "Balance cannot exceed £90" if exceed_limit?(amount)
+    @balance += amount
+  end
+
+  def exceed_limit?(amount)
+    (@balance + amount) > 90
+
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,20 +4,21 @@ describe OysterCard do
   subject(:oystercard) { described_class.new }
 
   it { is_expected.to(respond_to(:balance)) }
-  it {is_expected.to (respond_to(:top_up).with(1).argument)}
+  it { is_expected.to (respond_to(:top_up).with(1).argument) }
 
   describe '#balance' do
     it 'has a balance of zero' do
       expect(oystercard.balance).to(eq(0))
     end
   end
+
   describe '#top_up' do
-  it 'increases the balance with a given amount' do
-    oystercard.top_up 10
-    expect(oystercard.balance).to eq 10
-    end
     it 'can top_up the balance' do
-      expect{oystercard.top_up 5}.to change{oystercard.balance}.by 5
+      expect{ oystercard.top_up 5 }.to change{ oystercard.balance }.by 5
+    end
+    it 'raises an error if the top up will take the balance over £90' do
+      expect{ oystercard.top_up(100) }.to(raise_error("Balance cannot exceed £90"))
     end
   end
+
 end
