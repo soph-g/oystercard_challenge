5a76c04f3f7697c5f97e5e386a7b38531dde1e06
Sophie Gill <sophie-gill@hotmail.com>
2016-07-11 16:10:43 +0100

    tested for balance being zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,14 @@
 require 'oystercard'
 
 describe OysterCard do
+  subject(:oystercard) { described_class.new }
 
   it { is_expected.to(respond_to(:balance)) }
+
+  describe '#balance' do
+    it 'has a balance of zero' do
+      expect(oystercard.balance).to(eq(0))
+    end
+  end
 
 end
