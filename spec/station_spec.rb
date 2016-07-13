ec4652d040948c0461a5cd9f973c4ed092596cce
Sophie Gill <sophie-gill@hotmail.com>
2016-07-13 11:48:13 +0100

    adding station class with name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,15 @@
 require 'station'
 
 describe Station do
-  
+
+  subject(:station) { described_class.new("Old Street", 1) }
+
+  it "knows it's name" do
+    expect(station.name).to(eq("Old Street"))
+  end
+
+  it "knows it's zone" do
+    expect(station.zone).to(eq(1))
+  end
+
 end
